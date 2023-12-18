87b89936d561ba16543db99e629210f47e169412
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2023-12-18 23:39:31 +0200

    Major changes on code structure and added modify/delete button

--- Project/StockManagement.java
+++ Project/StockManagement.java
@@ -4,82 +4,242 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
 import Project.Main;
 
+/**
+ * This class is used to manage the stock of the store.
+ */
 public class StockManagement extends JFrame {
-	private JButton addProductButton;
-	private final JTextField categoryField;
-	private final JTextField barcodeField;
-	private final JTextField productNameField;
-	private final JTextField quantityField;
-	private final JTextField priceField;
+	private JTextField categoryField;
+	private JTextField barcodeField;
+	private JTextField productNameField;
+	private JTextField quantityField;
+	private JTextField priceField;
+	private JTextField newQuantityField;
+	private JTextField newPriceField;
 
+	/**
+	 * This method returns a JButton that adds a product to the stock.
+	 * @return = JButton that adds a product to the stock.
+	 */
+	private JButton getAddButton() {
+		JButton addProductButton = new JButton("Add Product");
+		addProductButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame addFrame = new JFrame("Add Product");
+				addFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				addFrame.setSize(400, 200);
+
+				JPanel addPanel = new JPanel();
+				addPanel.setLayout(new GridLayout(0, 2));
+
+				categoryField = new JTextField(20);
+				barcodeField = new JTextField(20);
+				productNameField = new JTextField(20);
+				quantityField = new JTextField(20);
+				priceField = new JTextField(20);
+
+				JButton confirmAddButton = new JButton("Confirm Add");
+				confirmAddButton.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if (categoryField.getText().isEmpty() || barcodeField.getText().isEmpty() || productNameField.getText().isEmpty() || quantityField.getText().isEmpty() || priceField.getText().isEmpty()) {
+							JOptionPane.showMessageDialog(null, "Please fill all the spaces with data.");
+						} else {
+							try {
+								String category = categoryField.getText();
+								String barcode = barcodeField.getText();
+								String name = productNameField.getText();
+
+								if (Main.insertIntoStockTable(category, new Product(barcode, name, Integer.parseInt(quantityField.getText()), Double.parseDouble(priceField.getText())))) {
+									JOptionPane.showMessageDialog(null, "Product added successfully");
+								} else {
+									JOptionPane.showMessageDialog(null, "Failed to add product. It may already exist in the stock.");
+								}
+							} catch (NumberFormatException | HeadlessException ex) {
+								throw new RuntimeException(ex);
+							}
+						}
+					}
+				});
+
+				addPanel.add(new JLabel("Category: "));
+				addPanel.add(categoryField);
+				addPanel.add(new JLabel("Barcode: "));
+				addPanel.add(barcodeField);
+				addPanel.add(new JLabel("Product Name: "));
+				addPanel.add(productNameField);
+				addPanel.add(new JLabel("Quantity: "));
+				addPanel.add(quantityField);
+				addPanel.add(new JLabel("Price: "));
+				addPanel.add(priceField);
+				addPanel.add(confirmAddButton);
+
+				addFrame.add(addPanel);
+				addFrame.setVisible(true);
+			}
+		});
+		return addProductButton;
+	}
+
+	/**
+	 * This method returns a JButton that modifies a product in the stock.
+	 * @return = JButton that modifies a product in the stock.
+	 */
+	private JButton getModifyButton() {
+		JButton modifyProductButton = new JButton("Modify Product");
+		modifyProductButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame modifyFrame = new JFrame("Modify Product");
+				modifyFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				modifyFrame.setSize(400, 200);
+
+				JPanel modifyPanel = new JPanel();
+				modifyPanel.setLayout(new GridLayout(0, 2));
+
+				categoryField = new JTextField(20);
+				barcodeField = new JTextField(20);
+				newQuantityField = new JTextField(20);
+				newPriceField = new JTextField(20);
+
+				JButton confirmModifyButton = new JButton("Confirm Modify");
+				confirmModifyButton.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if (categoryField.getText().isEmpty() || barcodeField.getText().isEmpty() || newQuantityField.getText().isEmpty() || newPriceField.getText().isEmpty()) {
+							JOptionPane.showMessageDialog(null, "Please fill all the spaces with data.");
+						} else {
+							try {
+								String category = categoryField.getText();
+								String barcodeToModify = barcodeField.getText();
+								int newQuantity = Integer.parseInt(newQuantityField.getText());
+								double newPrice = Double.parseDouble(newPriceField.getText());
+
+								Product product = Main.getProductDetails(barcodeToModify);
+								if (product != null) {
+									if (Main.modifyProduct(category, product, newQuantity, newPrice)) {
+										JOptionPane.showMessageDialog(null, "Product modified successfully");
+									} else {
+										JOptionPane.showMessageDialog(null, "Failed modifying product.");
+									}
+								} else {
+									JOptionPane.showMessageDialog(null,  "Product doesn't exist.");
+								}
+							} catch (NumberFormatException | HeadlessException ex) {
+								throw new RuntimeException(ex);
+							}
+						}
+						modifyFrame.dispose();
+					}
+				});
+
+				modifyPanel.add(new JLabel("Category: "));
+				modifyPanel.add(categoryField);
+				modifyPanel.add(new JLabel("Barcode to Modify: "));
+				modifyPanel.add(barcodeField);
+				modifyPanel.add(new JLabel("New Quantity: "));
+				modifyPanel.add(newQuantityField);
+				modifyPanel.add(new JLabel("New Price: "));
+				modifyPanel.add(newPriceField);
+				modifyPanel.add(confirmModifyButton);
+
+				modifyFrame.add(modifyPanel);
+				modifyFrame.setVisible(true);
+			}
+		});
+		return modifyProductButton;
+	}
+
+	/**
+	 * This method returns a JButton that deletes a product from the stock.
+	 * @return = JButton that deletes a product from the stock.
+	 */
+	private JButton getDeleteButton() {
+		JButton deleteProductButton = new JButton("Delete Product");
+		deleteProductButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame modifyFrame = new JFrame("Delete Product");
+				modifyFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				modifyFrame.setSize(400, 200);
+
+				JPanel modifyPanel = new JPanel();
+				modifyPanel.setLayout(new GridLayout(0, 2));
+
+				categoryField = new JTextField(20);
+				barcodeField = new JTextField(20);
+
+				JButton confirmDeleteButton = new JButton("Confirm Delete");
+				confirmDeleteButton.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if (categoryField.getText().isEmpty() || barcodeField.getText().isEmpty()) {
+							JOptionPane.showMessageDialog(null, "Please fill all the fields.");
+						} else {
+							try {
+								String category = categoryField.getText();
+								String barcode = barcodeField.getText();
+
+								Product product = Main.getProductDetails(barcode);
+								if (product != null) {
+									if (Main.deleteProduct(category, product)) {
+										JOptionPane.showMessageDialog(null, "Product deleted successfully.");
+									} else {
+										JOptionPane.showMessageDialog(null, "Failed to delete product");
+									}
+								} else {
+									JOptionPane.showMessageDialog(null, "Product doesn't exists.");
+								}
+							} catch (Exception ex) {
+								throw new RuntimeException(ex);
+							}
+						}
+						modifyFrame.dispose();
+					}
+				});
+				modifyPanel.add(new JLabel("Category: "));
+				modifyPanel.add(categoryField);
+				modifyPanel.add(new JLabel("Barcode to Delete: "));
+				modifyPanel.add(barcodeField);
+				modifyPanel.add(confirmDeleteButton);
+
+				modifyFrame.add(modifyPanel);
+				modifyFrame.setVisible(true);
+			}
+		});
+		return deleteProductButton;
+	}
+
+	/**
+	 * This is the constructor of the StockManagement class.
+	 */
 	public StockManagement() {
 		setTitle("Stock Management");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(400, 200);
 
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(0, 2)); // Using GridLayout for organized alignment
+		panel.setLayout(new GridLayout(0, 3));
 
-		categoryField = new JTextField(20);
-		barcodeField = new JTextField(20);
-		productNameField = new JTextField(20);
-		quantityField = new JTextField(20);
-		priceField = new JTextField(20);
-
-		addProductButton = new JButton("Add Product");
-		addProductButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String url = "jdbc:sqlite:/home/catalin/workspace/git/StockManagement/identifier.sqlite";
-
-				if (categoryField.getText().isEmpty() || barcodeField.getText().isEmpty() || productNameField.getText().isEmpty() || quantityField.getText().isEmpty() || priceField.getText().isEmpty()) {
-					JOptionPane.showMessageDialog(null, "Please fill all the spaces with data.");
-
-				} else {
-					try {
-						Connection connection = DriverManager.getConnection(url);
-						System.out.println("Connected successfully to database.");
-
-						// Get inserted data from interface
-						String category = categoryField.getText();
-						String barcode = barcodeField.getText();
-						String name = productNameField.getText();
-
-						// Main function for inserting into stock db
-						if (Main.insertIntoStockTable(category, new Product(barcode, name, Integer.parseInt(quantityField.getText()), Double.parseDouble(priceField.getText())))) {
-							JOptionPane.showMessageDialog(null, "Product added successfully");
-						} else {
-							JOptionPane.showMessageDialog(null, "Failed to add product. It may already exist in the stock.");
-						}
-					} catch (SQLException ex) {
-						JOptionPane.showMessageDialog(null, ex.getMessage());
-					}
-				}
-			}
-		});
-
-		panel.add(new JLabel("Category: "));
-		panel.add(categoryField);
-		panel.add(new JLabel("Barcode: "));
-		panel.add(barcodeField);
-		panel.add(new JLabel("Product Name: "));
-		panel.add(productNameField);
-		panel.add(new JLabel("Quantity: "));
-		panel.add(quantityField);
-		panel.add(new JLabel("Price: "));
-		panel.add(priceField);
+		JButton addProductButton = getAddButton();
+		JButton modifyProductButton = getModifyButton();
+		JButton deleteProductButton = getDeleteButton();
 
 		panel.add(addProductButton);
+		panel.add(modifyProductButton);
+		panel.add(deleteProductButton);
 
 		add(panel);
 		setVisible(true);
 	}
 
+	/**
+	 * This is the main method of the StockManagement class.
+	 * @param args = arguments.
+	 */
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
